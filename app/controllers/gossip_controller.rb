c45db6f9d999d4ca72b4214caa78a10784c10a6c
Quentin <berson.quentin@gmail.com>
2018-05-01 00:42:20 +0200

    add create and create

---
+++ app/controllers/gossip_controller.rb
@@ -0,0 +1,21 @@
+class GossipController < ApplicationController
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create 
+    @gossip = Gossip.new(anonymous_author: params["anonymous_author"], content: params["content"])
+    @gossip.save
+    redirect_to gossip_path(@user)
+  end
+
+  def show 
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy 
+  end
+end
