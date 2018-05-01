c7456c069a90b3153e99967cc8690c06e043c110
Quentin <berson.quentin@gmail.com>
2018-05-01 04:05:32 +0200

    ajout update and destroy

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,4 +1,5 @@
 class CommentController < ApplicationController
+
   def new
     @comment = Comment.new
   end
@@ -9,12 +10,27 @@ class CommentController < ApplicationController
     redirect_to gossip_path(@comment.gossip_id)
   end
 
+  def edit 
+    @comment = Comment.find(params[:id])
+  end
+
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    permitted_columns = params.require(:comment).permit(:anonymous_author, :body)
+    @comment.update_attributes(permitted_columns)
+
+    redirect_to gossip_path(@comment.gossip_id)
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  
+    redirect_to gossip_path(@gossip.gossip_id), notice: "Delete success"
   end
+
 end
